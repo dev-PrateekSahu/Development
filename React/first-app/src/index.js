8dc3f9cc2bc37330caa13c285eb36fa22aee9a2a
dev-PrateekSahu <prateeksahu75116@gmail.com>
2025-08-20 13:12:14 +0530

    Trying React

---
+++ React/first-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <h1 style={{color:"red"}}>Hello World!!</h1>
+);
